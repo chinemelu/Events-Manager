4d93e32b1dd92364dfab64fff1ab8f9913d1bc8e
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-11-21 06:17:23 +0100

    feat(sign-in-API): create a sign in API route [finishes #152807353]

--- server/test/test.js
+++ server/test/test.js
@@ -1,18 +1,6 @@
 import chai from 'chai';
 import chaihttp from 'chai-http';
-<<<<<<< Updated upstream
 import server from '../http/server';
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-import server from '../http/server';
-=======
-import server from '../../app';
->>>>>>> 86cc580465fb280e43feeccdb5abb3922bfcfec5
-=======
-import server from '../../app';
->>>>>>> 86cc580465fb280e43feeccdb5abb3922bfcfec5
->>>>>>> Stashed changes
 import db from '../models/index';
 
 
@@ -22,16 +10,6 @@ const { expect } = chai;
 
 describe('Users', () => {
   describe('POST: /api/v1/users/', () => {
-    beforeEach((done) => {
-      db.User.create({
-        username: 'testusername',
-        email: 'testemail@test.com',
-        isAdmin: false,
-        password: '@test1password',
-      });
-      done();
-    });
-
     afterEach((done) => {
       db.User.destroy({
         where: {}
@@ -49,7 +27,6 @@ describe('Users', () => {
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -64,7 +41,7 @@ describe('Users', () => {
           done();
         });
     });
-    
+
     it('it should not create a user if the username is fewer than 4 characters', (done) => {
       const user = {
         username: 'tes',
@@ -75,7 +52,6 @@ describe('Users', () => {
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -100,7 +76,6 @@ describe('Users', () => {
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -126,7 +101,6 @@ describe('Users', () => {
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -152,7 +126,6 @@ describe('Users', () => {
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -170,14 +143,13 @@ describe('Users', () => {
     it('it should not create a user if the username has space between characters', (done) => {
       const user = {
         username: 'test password',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -195,14 +167,13 @@ describe('Users', () => {
     it('it should not create a user if the email field is empty', (done) => {
       const user = {
         username: 'test',
-				email: null,
-				isAdmin: false,
+        email: null,
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -214,25 +185,24 @@ describe('Users', () => {
           done();
         });
     });
-  
+
     it('it should not create a user if the email is of an invalid format', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test',
-				isAdmin: false,
+        email: 'testemail@test',
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
           res.body.should.be.a('object');
           res.body.should.have.property('errors');
           res.body.errors.should.be.a('array');
-          res.body.errors[0].should.eql('The email you entered is invalid, please try again.')
+          res.body.errors[0].should.eql('The email you entered is invalid, please try again.');
           done();
         });
     });
@@ -241,14 +211,13 @@ describe('Users', () => {
        'contained in the email', (done) => {
       const user = {
         username: 'test',
-				email: 'Testemail@test.com',
-				isAdmin: false,
+        email: 'Testemail@test.com',
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -263,14 +232,13 @@ describe('Users', () => {
     it('it should not create a user if the password field is empty', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: null,
         reEnterPassword: null,
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -288,14 +256,13 @@ describe('Users', () => {
     it('it should not create a user if the password is fewer than 8 characters', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@Test1',
         reEnterPassword: '@Test1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -312,15 +279,14 @@ describe('Users', () => {
     it('it should not create a user if the password is greater than 100 characters', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: 'thispasswordistoolongthatIdontknowhowIwillgetitupto\n' +
           'ahundredIwilltrytodomyverybesttogetituptoahundredcharacters.',
         reEnterPassword: '@Test1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -338,14 +304,13 @@ describe('Users', () => {
     it('it should not create a user if the password has no lowercase characters', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@UPPERCASEPASSWORD1',
         reEnterPassword: '@UPPERCASEPASSWORD1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -362,14 +327,13 @@ describe('Users', () => {
     it('it should not create a user if the password has no uppercase characters', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@lowercasepassword1',
         reEnterPassword: '@lowercasepassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -386,14 +350,13 @@ describe('Users', () => {
     it('it should not create a user if the password does not contain a number', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@UPPERCASElowercasepassword',
         reEnterPassword: '@UPPERCASElowercasepassword'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -406,18 +369,17 @@ describe('Users', () => {
           done();
         });
     });
-  
+
     it('it should not create a user if the password has no special character', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: 'UPPERCASElowercasepassword1',
         reEnterPassword: 'UPPERCASElowercasepassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -434,14 +396,13 @@ describe('Users', () => {
     it('it should not create a user if the passwords do not match', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@Password1',
         reEnterPassword: '@differentPassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -452,18 +413,17 @@ describe('Users', () => {
           done();
         });
     });
-      
+
     it('it should create a user when all fields are completed correctly', (done) => {
       const user = {
         username: 'test',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
       chai.request(server)
         .post('/api/v1/users/')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(201);
@@ -498,7 +458,7 @@ describe('Users', () => {
       chai.request(server)
         .post('/api/v1/users/')
         .send(user)
-        .end((err, res) => {
+        .end(() => {
           chai.request(server)
             .post('/api/v1/users/')
             .send(user2)
@@ -528,7 +488,7 @@ describe('Users', () => {
       chai.request(server)
         .post('/api/v1/users/')
         .send(user)
-        .end((err, res) => {
+        .end(() => {
           chai.request(server)
             .post('/api/v1/users/')
             .send(user2)
@@ -575,7 +535,6 @@ describe('Users', () => {
       };
       chai.request(server)
         .post('/api/v1/users/login')
-        .set({ token: process.env.TEST_TOKEN })
         .send(user)
         .end((err, res) => {
           res.should.have.status(400);
@@ -590,8 +549,8 @@ describe('Users', () => {
       'not exist', (done) => {
       const user1 = {
         username: 'testusername',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@testPassword',
         reEnterPassword: '@testPassword'
       };
@@ -602,7 +561,7 @@ describe('Users', () => {
       chai.request(server)
         .post('/api/v1/users/')
         .send(user1)
-        .end((err, res) => {
+        .end(() => {
           chai.request(server)
             .post('/api/v1/users/login')
             .send(user2)
@@ -619,8 +578,8 @@ describe('Users', () => {
     it('it should not login a user if the password is incorrect', (done) => {
       const user1 = {
         username: 'testusername',
-				email: 'testemail@test.com',
-				isAdmin: false,
+        email: 'testemail@test.com',
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
@@ -629,11 +588,11 @@ describe('Users', () => {
         password: '@testPassword2'
       };
       chai.request(server)
-        .post('/api/v1/users/signup')
+        .post('/api/v1/users')
         .send(user1)
-        .end((err, res) => {
+        .end(() => {
           chai.request(server)
-            .post('/api/v1/users/signin')
+            .post('/api/v1/users/login')
             .send(user2)
             .end((err, res) => {
               res.should.have.status(401);
@@ -648,8 +607,8 @@ describe('Users', () => {
     it('it should login a user if the details are correct', (done) => {
       const user1 = {
         username: 'testusername',
-				email: 'testemail1@test.com',
-				isAdmin: false,
+        email: 'testemail1@test.com',
+        isAdmin: false,
         password: '@testPassword1',
         reEnterPassword: '@testPassword1'
       };
@@ -658,11 +617,11 @@ describe('Users', () => {
         password: '@testPassword1'
       };
       chai.request(server)
-        .post('/api/v1/users/signup')
+        .post('/api/v1/users')
         .send(user1)
-        .end((err, res) => {
+        .end(() => {
           chai.request(server)
-            .post('/api/v1/users/signin')
+            .post('/api/v1/users/login')
             .send(user2)
             .end((err, res) => {
               res.should.have.status(200);
