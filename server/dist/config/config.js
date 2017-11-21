11cbc8ecdf7b5820b5c1ecfd6769c4f197944b25
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-11-21 09:33:09 +0100

    feat(sign-in-API_route): configure config.js and .travis.yml file to make travisCI build application successfully

--- server/dist/config/config.js
+++ server/dist/config/config.js
@@ -11,10 +11,10 @@ module.exports = {
     host: '127.0.0.1'
   },
   test: {
-    username: 'postgres',
-    password: null,
-    database: 'events_manager_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres'
   },
   production: {
