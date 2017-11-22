b7eef050634063c7cb5d5e8ee702d78f702dc4e9
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-11-22 16:17:14 +0100

    feat(modify-event-API): construct routes and controllers to enable a user to modify an event

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -4,4 +4,6 @@ import createEventValidator from '../validation/createevent';
 import authenticatetoken from '../auth/authenticatetoken';
 
 const router = express.Router();
+
 router.post('/', createEventValidator, authenticatetoken, eventController.addEvent);
+router.put('/:id', createEventValidator, authenticatetoken, eventController.modifyEvent);
