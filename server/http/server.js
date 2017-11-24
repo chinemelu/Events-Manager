fc2c4f71bf0a7b46ffc3f13cf8024bc9476ef9da
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-11-24 16:02:20 +0100

    deploy heroku

--- server/http/server.js
+++ server/http/server.js
@@ -1,4 +1,4 @@
-import app from '../../app';
+import app from '../app';
 import userRoutes from '../routes/userRoute';
 import centerRoutes from '../routes/centerRoute';
 import eventRoutes from '../routes/eventRoute';
