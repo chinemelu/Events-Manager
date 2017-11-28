b7d45c5694384e3e4d07aa915d110655192278dd
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-11-28 17:51:51 +0100

    feat: create sign up react components

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../css/sign-in-form.scss';
+import '../css/sign-up-form.scss';
 import Navbar from './navbar.jsx';
 import Signinbody from './signinbody.jsx';
+import Signupbody from './signupbody.jsx';
+import Signupnavbar from './signupnavbar.jsx';
 
 const App = () => (
     <div>
