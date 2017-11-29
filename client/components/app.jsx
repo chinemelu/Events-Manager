879cc3211135fd5fffbc109788d445b36f116fbf
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-11-29 06:20:07 +0100

    create react components for the see all events centers page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../css/sign-in-form.scss';
 import '../css/sign-up-form.scss';
-import '../css/landingpagebody.scss'
+import '../css/landingpagebody.scss';
+import '../css/seealleventscenters.scss';
 import Navbar from './navbar.jsx';
 import Signinbody from './signinbody.jsx';
 import Signupbody from './signupbody.jsx';
 import Signupnavbar from './signupnavbar.jsx';
 import Landingpagebody from './landingpagebody.jsx'
+import Innernavbar from './inner-navbar.jsx'
+import Seealleventscenters from './seealleventscenters.jsx'
 
 const App = () => (
     <div>
-      <Navbar />
-      <Landingpagebody />
+      <Innernavbar />
+      <Seealleventscenters />
     </div>
 )
 
