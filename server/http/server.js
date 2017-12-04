8603e041456dc246d73a658410bc7e9a069169ab
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2017-12-04 05:12:27 +0100

    remove hard coded port 3000 and make port more flexible to get

--- server/http/server.js
+++ server/http/server.js
@@ -28,8 +28,8 @@ const port = process.env.PORT || 3000;
 
 app.set('port', port);
 
-const server = app.listen(port, () => {
-  console.log('listening to port 3000');
+const server = app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 export default server;
